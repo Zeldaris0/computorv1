d58ea3cec6d3cfa72afb784abf05114b7a871bfe
Mouad El azami <moel-aza@e3r1p12.1337.ma>
2022-06-11 18:49:16 +0100

    next , try to check if terms correct

--- computor.py
+++ computor.py
@@ -16,7 +16,7 @@ def sign_cleaner(term_list):
 
 def parser(poly):
     poly = poly.replace(" ", "")
-    allowed = "0123456789-+*^xX="
+    allowed = "0123456789-+*^xX"
     if not all(c in allowed for c in poly):
         print("ERROR404")
         return 0
@@ -31,10 +31,16 @@ def parser(poly):
 
 def main():
     if (len(sys.argv) > 2):
-        print "there is more that 1 arg"
+        print ("there is more that 1 arg")
         return 0
     else:
-        degree = parser(str(sys.argv[1]))
+        arr = str(sys.argv[1])
+        arr = arr.split("=")
+        if len(arr) != 2:
+            print("ERROR4044")
+            return 0
+        poly1 = parser(arr[0])
+        poly2 = parser(arr[1])
 
 if __name__ == "__main__":
     main()
