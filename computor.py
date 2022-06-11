0ce4dd7eed7c76d5e93cdd20808c0240c4384baa
Mouad El azami <moel-aza@e3r1p12.1337.ma>
2022-06-11 17:37:38 +0100

    parsinnnn

--- computor.py
+++ computor.py
@@ -3,26 +3,38 @@
 import sys
 import re
 
+def sign_cleaner(term_list):
+    termlen = len(term_list)
+    for idx, term in enumerate(term_list):
+        if term == "-" and idx < termlen-1:
+            term_list[idx+1] = "-" + term_list[idx+1]
+            term_list[idx] = None
+    
+    term_list = filter(None, term_list)
+    return term_list
 
-def my_function(poly):
-    poly_splited = re.split('- |+', poly)
-    print (poly_splited)
-    for x in poly_splited:
-        powerindex = x.find("^")
-        if x[powerindex + 1].isdigit():
-            if x[powerindex + 1] > 2:
-                return 0
-        else:
-            return 0
-    print("Hello from a function")
-    return 1
+
+def parser(poly):
+    poly = poly.replace(" ", "")
+    allowed = "0123456789-+*^xX="
+    if not all(c in allowed for c in poly):
+        print("ERROR404")
+        return 0
+
+    poly = re.sub("-\+|\+-", "-", poly)
+    poly = re.sub("--", "+", poly)
+    poly_splited = re.split("(?<!\*)([-])|(?<!\*)\+", poly)
+    poly_splited = filter(None, poly_splited)
+    term_list = sign_cleaner(poly_splited)
+    print (term_list)
+
 
 def main():
     if (len(sys.argv) > 2):
         print "there is more that 1 arg"
         return 0
     else:
-        degree = my_function(str(sys.argv[1]))
+        degree = parser(str(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
